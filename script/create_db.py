51f13e02602a8345edc9aa331cf2cb364d80d4b6
13120448819 <ly617615LY>
2022-11-24 10:38:18 +0800

    add root path

--- script/create_db.py
+++ script/create_db.py
@@ -5,14 +5,15 @@
 # @FileName : create_db.py
 # @Software : PyCharm
 import sys
+sys.path.append('/usr/local/pdf2docx')
 
 import pyrqlite.dbapi2 as dbapi2
 
 from constant import DB_HOST
-sys.path.append('/usr/local/pdf2docx')
+
 
 if __name__ == '__main__':
-    connection = dbapi2.connect(host='test1')
+    connection = dbapi2.connect(host=DB_HOST)
     connection.execute("""
     CREATE TABLE file(
        id             INTEGER        PRIMARY KEY     AUTOINCREMENT     NOT NULL,
